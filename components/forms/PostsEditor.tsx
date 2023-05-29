5367e01f1e6e58860fe55a145ed6268015ae9ecf
Raul Contreras <sarmancoder@gmail.com>
2023-05-29 11:56:14 +0200

    Configurar formulario con formik

--- components/forms/PostsEditor.tsx
+++ components/forms/PostsEditor.tsx
@@ -1,21 +1,31 @@
 import { Button, Card, CardContent, Grid, TextField, Typography } from "@mui/material";
+import { useFormik } from "formik";
 
 export default function PostsEditor () {
+    const formik = useFormik({
+        initialValues: {title: '', body: ''},
+        onSubmit: (values, {setSubmitting}) => {
+            setSubmitting(true)
+            console.log(values)
+            setSubmitting(false)
+        }
+    })
+
     return (
-        <Card elevation={0} component={'form'}>
+        <Card elevation={0} component={'form'} onSubmit={formik.handleSubmit}>
             <CardContent>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Typography variant="h5">Editor de posts</Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Título del post" fullWidth />
+                        <TextField name="title" onChange={formik.handleChange} label="Título del post" fullWidth />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Cuerpo del post" fullWidth />
+                        <TextField name="body" onChange={formik.handleChange} label="Cuerpo del post" fullWidth />
                     </Grid>
                     <Grid item xs={12}>
-                        <Button variant="contained" disableElevation>Guardar</Button>
+                        <Button type="submit" variant="contained" disableElevation>Guardar</Button>
                     </Grid>
                 </Grid>
             </CardContent>
