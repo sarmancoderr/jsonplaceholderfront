d7eb36da92aba420b0efffa287b514eb8beb69b1
Raul Contreras <sarmancoder@gmail.com>
2023-05-30 12:43:23 +0200

    Actualizar app bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,19 +18,9 @@ export default function RootLayout({
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
             <Toolbar>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                News
+                JsonPlaceholder front
               </Typography>
-              <Button color="inherit">Login</Button>
             </Toolbar>
           </AppBar>
         </Box>
